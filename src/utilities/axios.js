afb298e70857e5f0ec3d884e5e5094ea0776687a
Sujith Varughese <sujithv15@gmail.com>
2025-03-15 23:43:53 -0400

    updated to ai model

--- src/utilities/axios.js
+++ src/utilities/axios.js
@@ -11,4 +11,21 @@ export const openai = axios.create({
 	},
 });
 
+export const openAiStream = axios.create({
+	baseURL: 'https://api.openai.com/v1',
+	headers: {
+		'Content-Type': 'application/json',
+		'Authorization': `Bearer ${API_KEY}`,
+	},
+	method: 'POST',
+	responseType: 'stream',
+	onDownloadProgress: progressEvent => {
+		const sse = new EventSource('https://api.openai.com/v1/responses')
+		console.log(sse)
+		const xhr = progressEvent.event.target
+		const { responseText } = xhr
+		console.log("=====responseText======")
+	}
+})
+
 //		'OpenAI-Beta': 'assistants=v2'
