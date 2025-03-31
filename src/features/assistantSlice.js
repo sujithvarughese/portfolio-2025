e2796ee3c10f4812dded1b6e174389c6b006711b
Sujith Varughese <sujithv15@gmail.com>
2025-03-30 21:56:11 -0400

    update to ai model streaming

--- src/features/assistantSlice.js
+++ src/features/assistantSlice.js
@@ -55,10 +55,11 @@ export const fetchAiStream = createAsyncThunk('assistant/fetchAiStream', async (
       },
       body: JSON.stringify({
         model: "gpt-4o-mini",
+        instructions: "You are a helpful assistant for a portfolio website to help users get facts about the subject, Sujith Varughese. Responses should be no more than 50 words. If asked a question not related to his professional experience, politely explained that you are trained to give information about his professional information only. Do not mention that the information is not mentioned in the documents. Use light humor when needed.",
         tools: [{
           type: "file_search",
           vector_store_ids: [import.meta.env.VITE_VECTOR_STORE_ID],
-          max_num_results: 20
+          max_num_results: 10
         }],
         input: query,
         stream: true,
