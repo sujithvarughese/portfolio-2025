ba44fddf36f12aa1ea36fc098f5e80009a50f205
Sujith Varughese <sujithv15@gmail.com>
2025-09-15 11:35:13 -0400

    added magnification lens for project images

--- src/components/ui/timeline.tsx
+++ src/components/ui/timeline.tsx
@@ -10,6 +10,7 @@ import {Anchor, Avatar, Button, Flex, Image, Tooltip} from "@mantine/core";
 import { logos } from '../IconPill'
 import {LiaGithub, LiaRocketSolid} from "react-icons/lia";
 import {FaAppStore} from "react-icons/fa";
+import ProjectImage from "../ProjectImage.jsx";
 
 interface TimelineEntry {
   date: string;
@@ -85,14 +86,8 @@ export const Timeline = ({ data }: { data: TimelineEntry[] }) => {
                 {item.heading}
               </p>
               <div className="grid grid-cols-2 gap-4">
-                {item.images?.map(image =>
-                  <Image
-                    src={image}
-                    alt="hero template"
-                    width={500}
-                    height={500}
-                    className="h-20 w-full rounded-lg object-cover shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset] md:h-44 lg:h-60"
-                  />
+                {item.images?.map((image, index) =>
+                  <ProjectImage key={index} image={image} />
                 )}
               </div>
               <Flex gap={12} justify="center" wrap="wrap" py={16}>
